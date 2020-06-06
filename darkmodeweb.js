ca7f2a90fc4df84053d927de497f1e9f8ff6cfa9
Numan Zahid <labs.zahid@gmail.com>
2020-06-06 17:58:36 +0500

    code fix

--- darkmodeweb.js
+++ darkmodeweb.js
@@ -1,81 +1,71 @@
-//select class of icon container
+//write HTML that should be added to the page. Only needed when you have noe control over HTML of the page. Select wrapper where icon should be added.
 var setIconContainerClass = document.querySelector('.add-btn');
-//write html the should be inseted
 var insertIconHTML = '<button class="web-dark-btn" type="button" onclick="btnThemeSwitch();" title="Toggle light mode"><svg aria-hidden="true" class="svg-icon" width="24" height="24"><path d="M18.925 4.075c4.1 4.1 4.1 10.75 0 14.85s-10.75 4.1-14.85 0-4.1-10.75 0-14.85 10.75-4.1 14.85 0zM17.51 17.51a8.5 8.5 0 000-12.02L5.49 17.51a8.5 8.5 0 0012.02 0z"></path></svg></button>';
 setIconContainerClass.insertAdjacentHTML('beforeend', insertIconHTML);
 
-// mode changer
 var body = document.getElementsByTagName("body")[0];
-// these two listeners can go...
-window.matchMedia("(prefers-color-scheme: light)").addListener(useBrowserScheme);
-window.matchMedia("(prefers-color-scheme: dark)").addListener(useBrowserScheme);
+// listener for prefers-color-scheme
+window.matchMedia("(prefers-color-scheme: light)").addListener(applyBrowserScheme);
+window.matchMedia("(prefers-color-scheme: dark)").addListener(applyBrowserScheme);
 
-addStorageColor();
-
-window.addEventListener('storage', function(e) {
-    if (localStorage.darkmodeweb) {
-        // console.log('storage event');
-        addStorageColor();
-    }
-});
+applyStorageScheme();
 
 // callback functions
-function addStorageColor() {
-    if (localStorage.darkmodeweb === 'dark') {
+function applyStorageScheme() {
+    if (localStorage.getItem('theme') === 'dark') {
         themeSwitch('dark');
-        // console.log('User preference Dark');
-    } else if (localStorage.darkmodeweb === 'light') {
+    } else if (localStorage.getItem('theme') === 'light') {
         themeSwitch('light');
-        // console.log('User preference Light');
-    } else if (localStorage.darkmodeweb == null) {
-        useBrowserScheme();
-        // console.log('Browser color preference');
+    } else if (localStorage.getItem('theme') == null) {
+        applyBrowserScheme();
     }
 }
-
-function useBrowserScheme() {
+function applyBrowserScheme() {
     var browserPreferslight = window.matchMedia && window.matchMedia("(prefers-color-scheme: light)").matches;
     var browserPrefersdark = window.matchMedia && window.matchMedia("(prefers-color-scheme: dark)").matches;
-    if (browserPreferslight && localStorage.darkmodeweb == null) {
-        themeSwitchBrowser('light');
-    } else if (browserPrefersdark && localStorage.darkmodeweb == null) {
-        themeSwitchBrowser('dark');
+    if (browserPreferslight && localStorage.getItem('theme') == null) {
+        browserSwitch('light');
+    } else if (browserPrefersdark && localStorage.getItem('theme') == null) {
+        browserSwitch('dark');
     } else {
-        themeSwitchBrowser('dark');
+        browserSwitch('dark');
     }
 }
-
-function btnThemeSwitch() {
-    var hasDark = body.classList.contains('web-dark');
-    var hasLight = body.classList.contains('web-light');
-    if (hasLight) {
-        themeSwitch('dark');
-        localStorage.darkmodeweb = 'dark';
-    } else if (hasDark) {
-        themeSwitch('light');
-        localStorage.darkmodeweb = 'light';
+function themeSwitch(color) {
+    if (color === 'dark') {
+        body.classList.add("web-dark", "dark-forced");
+        body.classList.remove("web-light", "light-forced");
+    } else if (color === 'light') {
+        body.classList.remove("web-dark", "dark-forced");
+        body.classList.add("web-light", "light-forced");
     }
 }
-
-function themeSwitch(colorScheme) {
-    if (colorScheme === 'dark') {
-        body.classList.add("web-dark");
-        body.classList.add("dark-forced");
-        body.classList.remove("web-light");
-        body.classList.remove("light-forced");
-    } else if (colorScheme === 'light') {
-        body.classList.remove("web-dark");
-        body.classList.remove("dark-forced");
-        body.classList.add("web-light");
-        body.classList.add("light-forced");
-    }
-}
-function themeSwitchBrowser(browserColor) {
-    if (browserColor === 'light'){
+function browserSwitch(color) {
+    if (color === 'light'){
         body.classList.toggle("web-light", true);
         body.classList.toggle("web-dark", false);
-    } else if (browserColor === 'dark') {
+    } else if (color === 'dark') {
         body.classList.toggle("web-dark", true);
         body.classList.toggle("web-light", false);
     }
 }
+//button function
+function btnThemeSwitch() {
+    if (body.classList.contains('web-light')) {
+        themeSwitch('dark');
+        localStorage.setItem('theme', 'dark');
+    } else if (body.classList.contains('web-dark')) {
+        themeSwitch('light');
+        localStorage.setItem('theme', 'light');
+    }
+}
+// sync between tabs // listener for storage
+window.addEventListener('storage', function(e) {
+    if (localStorage.getItem('theme')) {
+        if (localStorage.getItem('theme') === 'dark') {
+            themeSwitch('dark');
+        } else if (localStorage.getItem('theme') === 'light') {
+            themeSwitch('light');
+        }
+    }
+});
